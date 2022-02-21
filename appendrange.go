f6331a7be6303737faee73941a7d9db5fae0ce89
student60 <talison.pt@gmx.fr>
2022-02-21 04:42:16 -0500

    My very first commit

--- appendrange.go
+++ appendrange.go
@@ -1,7 +1,6 @@
 package piscine
 
 func AppendRange(min, max int) []int {
-<<<<<<< HEAD
 	if min >= max {
 		var tab []int = nil
 		return tab
@@ -14,17 +13,3 @@ func AppendRange(min, max int) []int {
 		return tab
 	}
 }
-=======
-        if min >= max {
-                var tab []int = nil
-                return tab
-        }else{
-                tab := []int{}
-		
-                for i:= min;i < max;i++ {
-                        tab = append(tab,i)
-                }
-                return tab
-        }
-}
->>>>>>> d0fdce4a882dfa96c1d2f9ad9692d94809dc69a6
