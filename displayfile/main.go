f567b6c70e02799e8c5a63cce543983fa9313546
student60 <talison.pt@gmx.fr>
2022-02-21 13:35:46 -0500

    My very first commit

--- displayfile/main.go
+++ displayfile/main.go
@@ -12,19 +12,16 @@ func main() {
 	for i := range arguments {
 		length = i + 1
 	}
-	if length > 2 {
-		fmt.Print("Too many arguments")
-		fmt.Print("\n")
+	if length > 1 {
+		fmt.Println("Too many arguments")
 	} else if length == 0 {
-		fmt.Print("File name missing")
-		fmt.Print("\n")
+		fmt.Println("File name missing")
 	} else if arguments[0] == "quest8.txt" {
 		content, err := ioutil.ReadFile(arguments[0])
 		if err != nil {
-			fmt.Print(err.Error())
-			fmt.Print("\n")
+			fmt.Println(err.Error())
 			return
 		}
-		fmt.Print(string(content))
+		fmt.Println(string(content))
 	}
 }
