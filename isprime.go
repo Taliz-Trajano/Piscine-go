447b4865bcd563cad0fad57fe3d6203525be1df6
student60 <talison.pt@gmx.fr>
2022-02-22 07:01:06 -0500

    My very first commit

--- isprime.go
+++ isprime.go
@@ -1,19 +1,19 @@
 package piscine
 
-import "math"
-
 func IsPrime(nb int) bool {
-	if nb < 2 {
+	if nb <= 1 {
 		return false
-	} else if 2 == nb || 3 == nb {
-		return true
-	} else {
-		limit := math.Sqrt(float64(nb))
-		for i := 2; i <= int(limit); i++ {
-			if nb%i == 0 {
-				return false
-			}
-		}
+	}
+	if nb == 2 {
 		return true
 	}
+	if nb%2 == 0 {
+		return false
+	}
+	for i := 3; i <= nb/3; i += 2 {
+		if nb%i == 0 {
+			return false
+		}
+	}
+	return true
 }
