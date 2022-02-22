43e47a2f82853d8abe964070514995b61e689dbc
student60 <talison.pt@gmx.fr>
2022-02-22 06:53:48 -0500

    My very first commit

--- sqrt.go
+++ sqrt.go
@@ -1,14 +1,20 @@
 package piscine
 
+import "math"
+
 func Sqrt(nb int) int {
-	if nb <= 0 {
+	if nb < 0 || math.MaxInt16 < nb {
 		return 0
 	} else {
-		for i := 0; i < nb/2; i++ {
-			if i*i == nb {
-				return i
+		for i := 0; i < 101; i++ {
+			if nb == i*i {
+				nb = i
+				break
+			} else if i*i > nb {
+				nb = 0
+				break
 			}
 		}
 	}
-	return 1
+	return nb
 }
