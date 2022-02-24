20c3514fb31d23688b3897a81f02329be6ebe9cf
student60 <talison.pt@gmx.fr>
2022-02-24 22:48:29 +0100

    My very first commit

--- enigma.go
+++ enigma.go
@@ -1,23 +1,11 @@
 package piscine
 
-// this function will put a in c; c in d; d in b and b in a
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-	valc := *******c
+	permC := *******c
+	permD := ****d
+	permB := *b
 	*******c = ***a
-	vald := ****d
-	****d = valc
-	valb := *b
-	*b = vald
-	***a = valb
-}
-
-// Helper function used in the test for checking the function Enigma()
-func Decript(a ***int, b *int, c *******int, d ****int) {
-	vala := ***a
-	***a = *******c
-	valb := *b
-	*b = vala
-	vald := ****d
-	****d = valb
-	*******c = vald
+	****d = permC
+	*b = permD
+	***a = permB
 }
