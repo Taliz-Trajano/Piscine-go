535e2cac2add73068533193795b96d9cec318504
student60 <talison.pt@gmx.fr>
2022-02-24 07:19:23 -0500

    My very first commit

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -4,17 +4,14 @@ func CollatzCountdown(start int) int {
 	if start <= 0 {
 		return -1
 	}
-
-	steps := 1
-
+	count := 0
 	for start != 1 {
 		if start%2 == 0 {
 			start = start / 2
 		} else {
-			start = 3*start + 1
+			start = (start * 3) + 1
 		}
-		steps++
+		count++
 	}
-
-	return steps
+	return count
 }
