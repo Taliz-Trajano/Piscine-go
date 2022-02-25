d4d69135e4b3865c3fbf7a4d536a7833d0bfeb0b
student60 <talison.pt@gmx.fr>
2022-02-25 02:51:25 +0100

    My very first commit

--- activebits.go
+++ activebits.go
@@ -1,10 +1,10 @@
 package piscine
 
-func ActiveBits(n int) uint {
-	compte := 0
-	for n > 0 {
-		compte += n & 1
-		n >>= 1
+func ActiveBits(n int) int {
+	result := 0
+	for ; n > 1; n = n / 2 {
+		result += n % 2
 	}
-	return uint(compte)
+	result += n
+	return int(result)
 }
